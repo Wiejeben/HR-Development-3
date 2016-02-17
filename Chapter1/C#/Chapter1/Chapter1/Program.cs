6e01ad42652ea328dcee0350796c10a8934d817b
Maarten de Graaf <wiejeben@gmail.com>
2016-02-17 10:26:16 +0100

    Complete task 1.1.1

---
+++ Chapter1/C#/Chapter1/Chapter1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task1();
+
+            // Do not automatically leave application
+            Console.ReadKey(false);
+        }
+
+        // 1. Translate the Python-program below to Java or C#:
+        static void task1()
+        {
+            string result = "";
+
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    result = result + "*";
+                }
+
+                result = result + "\n";
+            }
+
+            Console.Write(result);
+        }
+    }
+}
