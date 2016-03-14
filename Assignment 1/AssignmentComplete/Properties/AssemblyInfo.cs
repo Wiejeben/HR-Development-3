d81346c0eb9fe349a7fd839d0a35d4af3568b005
Maarten de Graaf <maarten@webtraders.nl>
2016-03-14 20:33:00 +0100

    Initial commit Assignment 1

--- Assignment 1/AssignmentComplete/Properties/AssemblyInfo.cs
+++ Assignment 1/AssignmentComplete/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Chapter1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AssignmentComplete")]
+[assembly: AssemblyProduct("AssignmentComplete")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Chapter1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9f42a75f-164a-45b8-914b-4e61f4fd1707")]
+[assembly: Guid("d494fe62-2226-485d-9aa0-51055852f247")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.4.0.456")]
+[assembly: AssemblyFileVersion("3.4.0.456")]
